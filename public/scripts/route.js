052ec6dcab5508ad2c10d48cbab11f571887eac7
Noah Israel <noahmvf@gmail.com>
2018-05-19 14:10:44 -0700

    background-scaling

---
+++ public/scripts/route.js
@@ -0,0 +1,16 @@
+'use strict';
+// page('/', ctx => app.mapView.initGeoCode());
+page('/designs', showDesigns());
+// page('/websites', disAbout);
+// page('/about', disAbout);
+
+
+function showDesigns () {
+  $('#headline-container').hide();
+  $('#gallery-main').hide();
+  $('#websites').hide();
+  $('#my-story').hide();
+}
+
+
+
